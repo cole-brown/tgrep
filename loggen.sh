7fb5ba3083c68e9508aa3b86e70c71229f138c85
Cole Brown <git@spydez.com>
2011-02-11 23:41:35 -0500

    beefed up loggen; time grokking in tgrep

--- loggen.sh
+++ loggen.sh
@@ -3,25 +3,26 @@
 #bump to ~20000
 #MAX_LOGS=10
 
-MAX_SEC_APART=10
+MAX_SEC_APART=5
+MAX_SAME_LOG=30
 INITIAL_TIMESTAMP=1234567890
-END_TIMESTAMP=$INITIAL_TIMESTAMP + 90000 # one day, one hour later
+END_TIMESTAMP=0
+((END_TIMESTAMP=INITIAL_TIMESTAMP + 90000)) # one day, one hour later
 
 # switch to BLAH_BLAH_BLAH when done testing
 TEST_BLAH="[rest of log]"
 BLAH_BLAH_BLAH="web03 haproxy[1631]: 10.350.42.161:58625 [10/Feb/2011:10:59:49.089] frontend pool3/srv28-5020 0/138/0/19/160 200 488 - - ---- 332/332/13/0/0 0/15 {Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.7) Gecko/20100713 Firefox/3.6.7|www.reddit.com|http://www.reddit.com/r/pics/?count=75&after=t3_fiic6|201.8.487.192|17.86.820.117|} \"POST /api/vote HTTP/1.1\""
 
 logline() {
-  date -r $1 | awk -v f=2 -v t=4 '{ for (i=f; i<=t;i++) printf("%s%s", $i,(i==t) ? " " : OFS) }'
-  echo $BLAH_BLAH_BLAH
+  for i in {1..$1}
+  do
+    date -r $2 | awk -v f=2 -v t=4 '{ for (i=f; i<=t;i++) printf("%s%s", $i,(i==t) ? " " : OFS) }'
+    echo $BLAH_BLAH_BLAH
+  done
 }
 
-randsec() {
-  python -c "import random; print random.randrange(0,$MAX_SEC_APART);"
-}
-
-randrepeat() {
-  python -c "import random; print random.randrange(0,$MAX_SEC_APART);"
+rand() {
+  python -c "import random; print random.randrange($1,$2);"
 }
 
 
@@ -29,13 +30,13 @@ timestamp=$INITIAL_TIMESTAMP
 #logline $timestamp #start @ initial timestamp
 #for ((i = 0; i < $MAX_LOGS - 1; i++))
 #do
-#  ((timestamp = timestamp + `rand`))
+#  ((timestamp = timestamp + `rand $MAX_SEC_APART`))
 #  logline $timestamp
 #done
 
-#while [ $timestamp -lt $END_TIMESTAMP ]; do
-#  logline $timestamp
-#  ((timestamp = timestamp + `rand`))
-#done
+while [ $timestamp -lt $END_TIMESTAMP ]; do
+  logline `rand 1 $MAX_SAME_LOG` $timestamp
+  ((timestamp = timestamp + `rand 0 $MAX_SEC_APART`))
+done
 
-echo "Fix while loop so that some logs are repeated via randrepeoat() (which needs implementing)."
+#echo "Fix while loop so that some logs are repeated via randrepeoat() (which needs implementing)."
--- tgrep.py
+++ tgrep.py
@@ -55,36 +55,54 @@ from datetime import datetime
 MORE_THAN_ONE_LINE = 500 # bytes
 SEEK_BYTES = 376*9000 + 50
 
-DEFAULT_LOG = "h.txt"#"loggen.log" # //! "/log/haproxy.log" 
+DEFAULT_LOG = "loggen.log" # //! "/log/haproxy.log" 
 
 def doShit2(path_to_log):
   # whine if log file doesn't exists
   if not os.path.isfile(path_to_log):
     print "file '%s' does not exist" % path_to_log
 
+  # //! only here temp.
+  # Feb 13 23:33 (whole minute)
+  times = [datetime(2011, 2, 13, 23, 33, 00), datetime(2011, 2, 13, 23, 34, 00)]
+
   # for single vs multi, just bump guesses down to 1.
-  seek_guesses = [2,4]
-  guess_times  = Queue()
+  seek_guesses = [0,376*9000 + 50,376*5000 + 50,376*3000 + 50]
+  guess_results  = Queue()
   children = []
   with open(path_to_log, 'r') as log:
     for seek_loc in seek_guesses:
-      p = Process(target=workerBee, args=(log,seek_loc,guess_times))
+      p = Process(target=pessismistic_search, args=(log, seek_loc, times, guess_results))
       p.start()
       children.append(p)
     for child in children:
       child.join()
-      print guess_times.get()
+      print guess_results.get()
     
-def workerBee(file, seek_loc, guess_times):
-  file.seek(seek_loc)
-  guess_times.put([seek_loc, file.read(2)]) # read more
+# Reads only a little and checks only the first timestamp. Better when it's way off base.
+def pessismistic_search(log, seek_loc, times, results):
+  log.seek(seek_loc)
+  chunk = log.read(MORE_THAN_ONE_LINE)
+  # find the nearest newline so we can find the timestamp
+  nl_index = chunk.find("\n")
+  if nl_index == -1:
+    results.put([seek_loc, -1])
+    return
+    # //! better error case?
+  nl_index += 1 # get past the newline
+
+  # find the first bit of the line, e.g. "Feb 14 05:52:12 web0"
+  # split it on the whitespace, e.g. ["Feb", "14", "05:52:12", "web0"]
+  # join the first three back together again with ' ' as the seperator
+  time = ' '.join(chunk[nl_index:nl_index+20].split()[:3]) # //! need to research a better (faster?) way to do this
+
+  cmp_results = [time_cmp(time, times[0]),
+                 time_cmp(time, times[1])]
+
+  results.put([seek_loc, time, cmp_results])
 
 def time_cmp(time_str, desired):
   log_time = datetime.strptime(time_str + str(datetime.now().year), "%b %d %H:%M:%S%Y")
-  print "log: ", 
-  print log_time
-  print "desired: ",
-  print desired
   if log_time > desired:
     return 1 # 1 means "KEEP GOING DUDE!"
   elif log_time == desired:
@@ -92,51 +110,47 @@ def time_cmp(time_str, desired):
   else:
     return -1 # Too far! Pull back!
 
-def doShit(path_to_log):
-  children = []
-
-  # whine if log file doesn't exists
-  if not os.path.isfile(path_to_log):
-    print "file '%s' does not exist" % path_to_log
-
-  # //! state assumptions
-  seek_guesses = [0,0,0]
-  seek_g2 = []
-
-  with open(path_to_log, 'r') as log: 
-#    log.seek(SEEK_BYTES)
-    for seek_loc in seek_guesses:
-      pid = os.fork()
-      if pid is not 0: # this is the parent 
-        children.append(pid)
-      else: # these are the children
-        log.seek(0)
-        chunk = log.read(2)
-#       chunk = log.read(MORE_THAN_ONE_LINE)
-        print chunk
-        chunk = log.read(2)
-        print chunk
-        # find next date, compare
-        seek_g2.append(42)
-        os._exit(0)
-  
-    for child in children:
-      os.waitpid(child, 0)
-
+#def doShit(path_to_log):
+#  children = []
+#
+#  # whine if log file doesn't exists
+#  if not os.path.isfile(path_to_log):
+#    print "file '%s' does not exist" % path_to_log
+#
+#  # //! state assumptions
+#  seek_guesses = [0,0,0]
+#  seek_g2 = []
+#
+#  with open(path_to_log, 'r') as log: 
+##    log.seek(SEEK_BYTES)
+#    for seek_loc in seek_guesses:
+#      pid = os.fork()
+#      if pid is not 0: # this is the parent 
+#        children.append(pid)
+#      else: # these are the children
+#        log.seek(0)
+#        chunk = log.read(2)
+##       chunk = log.read(MORE_THAN_ONE_LINE)
+#        print chunk
+#        chunk = log.read(2)
+#        print chunk
+#        # find next date, compare
+#        seek_g2.append(42)
+#        os._exit(0)
+#  
+#    for child in children:
+#      os.waitpid(child, 0)
+#
 
 
 if __name__ == '__main__':
   now = datetime.today() # today() instead of now() to lose TZ info
   now.replace(microsecond=0)
   # get min and max via min=now and replace()
-  min = datetime(2011, 2, 1, 13, 34, 43)
-  print time_cmp("Feb  9 14:34:43", min)
-  min = datetime(2011, 2, 9, 14, 34, 43)
-  print time_cmp("Feb  9 14:34:43", min)
-  min = datetime(2011, 2, 9, 15, 34, 43)
-  print time_cmp("Feb  9 14:34:43", min)
+#  min = datetime(2011, 2, 1, 13, 34, 43)
+#  print time_cmp("Feb  9 14:34:43", min)
 
-#  doShit2(DEFAULT_LOG) # //! change this...
+  doShit2(DEFAULT_LOG) # //! change this...
 
   # parse input
   # figure out which file to open
