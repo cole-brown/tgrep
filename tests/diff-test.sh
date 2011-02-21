1e44b8a175cd71176fac7a12d7b4feefbcd58221
Cole Brown <git@spydez.com>
2011-02-21 02:51:59 -0500

    updated and moved grep test

--- tests/diff-test.sh
+++ tests/diff-test.sh
@@ -4,10 +4,10 @@
 # called '.log' to avoid getting added to git repo
 
 echo -e "\n\nTGREP"
-time ./tgrep -c config.py --ee $1 > x-tmp-diff-tgrep.log
+time ../tgrep -c ../config.py --ee ../biglog.log $1 > x-tmp-diff-tgrep.log
 echo -e "\n\nBORING GREP"
 date
-nice time grep " $1" biglog.log > x-tmp-diff-grep.log
+nice time grep " $1" ../biglog.log > x-tmp-diff-grep.log
 echo -e "\n\nDIFF"
 time diff -au x-tmp-diff-grep.log x-tmp-diff-tgrep.log > x-tmp-diff-diff.log
 echo -e "\n\n"
